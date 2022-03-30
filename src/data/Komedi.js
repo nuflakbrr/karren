6fdb8b3f1b9257027d62047703865a183682e1b4
IvanderDzaky <75924357+IvanderDzaky@users.noreply.github.com>
2022-03-30 14:42:31 +0800

    add team component

---
+++ src/data/Komedi.js
@@ -0,0 +1,43 @@
+const komedi = {
+    komedis: [
+        {
+            id: 1,
+            img: "https://naufalakbar.me/images/avatar.jpg",
+            name: "Naufal Akbar Nugroho",
+            job: "Backend, Frontend",
+            desc: "tralala trilili",
+            github: "https://github.com/nuflakbrr",
+            instagram: "https://instagram.com/kbrnugroho",
+            website: "https://naufalakbar.me",
+        },
+        {
+            id: 2,
+            img: "https://naufalakbar.me/images/avatar.jpg",
+            name: "Ivander Dzaky Khairullah",
+            job: "Frontend",
+            desc: "tralala trilili",
+            github: "https://github.com/IvanderDzaky",
+            instagram: "https://instagram.com/ivander_dzaky",
+        },
+        {
+            id: 3,
+            img: "https://naufalakbar.me/images/avatar.jpg",
+            name: "Regina Adristya",
+            job: "Backend",
+            desc: "tralala trilili",
+            github: "https://github.com/reginaadristya",
+            instagram: "https://instagram.com/reginaadristya",
+        },
+        {
+            id: 4,
+            img: "https://naufalakbar.me/images/avatar.jpg",
+            name: "Revalina Santosa",
+            job: "Frontend",
+            desc: "tralala trilili",
+            github: "https://github.com/Reprepaa",
+            instagram: "https://instagram.com/revalinaaas",
+        },
+    ]
+}
+
+export default komedi
