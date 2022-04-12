31288945883ce2382a5704b6c3bad58d14c86e9e
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-12 21:08:20 +0700

    improve

--- src/components/SidebarAdmin.jsx
+++ src/components/SidebarAdmin.jsx
@@ -1,29 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import jwtDecode from 'jwt-decode'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { AiOutlineClose } from 'react-icons/ai'
 import { FaHome, FaUserAlt, FaPowerOff } from 'react-icons/fa'
 
-export default function SidebarAdmin() {
+export default function Sidebar() {
     const [collapseShow, setCollapseShow] = useState('hidden')
 
-    useEffect(() => {
-        const token = localStorage.getItem('token')
-        if (token) {
-            const decoded = jwtDecode(token)
-            if (decoded.role === 'superadmin') {
-                window.location.href = '/admin/dashboard'
-            } else if (decoded.role === 'admin') {
-                window.location.href = '/admin/dashboard'
-            } else {
-                window.location.href = '/admin/login'
-            }
-        }
-        // else if (token === null) {
-        //     window.location.href = '/admin/login'
-        // }
-    }, [])
+    const token = localStorage.getItem('token')
+    const role = localStorage.getItem('role')
+
+    // If token and role not exist, this condition will redirect to login page
+    if (!token && !role) {
+        window.location.href = '/admin/login'
+    }
+
+    const logOut = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('role')
+        window.location.href = '/admin'
+    }
 
     const activeClass = 'text-white text-xs uppercase py-3 font-bold flex'
     const inActiveClass = 'text-gray-300 hover:text-gray-400 text-xs uppercase py-3 font-bold flex'
@@ -40,11 +35,11 @@ export default function SidebarAdmin() {
         <>
             <nav className='md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-green-700 flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6'>
                 <div className='md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto'>
-                    <h1 className='md:block text-left md:pb-2 text-white mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0'>
-                        Hai, {name}
-                    </h1>
+                    <NavLink className='md:block text-left md:pb-2 text-white mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0' to='/'>
+                        Hai, {localStorage.getItem('role')}
+                    </NavLink>
 
-                    <button className='cursor-pointer text-white focus:text-white md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent' type='button' onClick={() => setCollapseShow('bg-gray-900 m-2 py-3 px-6')}>
+                    <button className='cursor-pointer text-white focus:text-white md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent' type='button' onClick={() => setCollapseShow('bg-green-900 m-2 py-3 px-6')}>
                         <svg
                             className='block h-6 w-6'
                             stroke='currentColor'
@@ -67,11 +62,11 @@ export default function SidebarAdmin() {
                         <div className='md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200'>
                             <div className='flex flex-wrap'>
                                 <div className='w-6/12'>
-                                    <h1 className='md:block text-left text-white md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0'>
-                                        Hai, [admin]
-                                    </h1>
+                                    <NavLink className='md:block text-left text-white md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0' to='/'>
+                                        Hai, {localStorage.getItem('role')}
+                                    </NavLink>
                                 </div>
-                                <div className='w-6 /12 flex justify-end'>
+                                <div className='w-6/12 flex justify-end'>
                                     <button type='button' className='cursor-pointer text-white md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent' onClick={() => setCollapseShow('hidden')}>
                                         <AiOutlineClose />
                                     </button>
@@ -81,7 +76,7 @@ export default function SidebarAdmin() {
 
                         {/* Navigation */}
                         <h6 className='md:min-w-full text-slate-300 text-xs uppercase font-bold block mb-1'>
-                            Akses
+                            Aksesibilitas
                         </h6>
 
                         <ul className='md:flex-col md:min-w-full flex flex-col list-none'>
@@ -103,14 +98,14 @@ export default function SidebarAdmin() {
                         </h6>
 
                         <ul className='md:flex-col md:min-w-full flex flex-col list-none md:mb-4'>
-                            <li className='inline-flex'>
-                                <NavLink className={activeClassses('/')} to='/'>
+                            <li className='items-center'>
+                                <NavLink className={activeClassses('#')} to='#'>
                                     <FaUserAlt className='mr-2 text-lg' /> Petugas
                                 </NavLink>
                             </li>
 
-                            <li className='inline-flex'>
-                                <NavLink className={activeClassses('/about')} to='/about'>
+                            <li className='items-center'>
+                                <NavLink className={activeClassses('/admin')} to='/admin' onClick={logOut}>
                                     <FaPowerOff className='mr-2 text-lg' /> Keluar
                                 </NavLink>
                             </li>
